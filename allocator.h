96ae3d0d7ec0007ae36dcd18056708100990f40b
ddomingos1006 <64104768+ddomingos1006@users.noreply.github.com>
2025-09-25 13:21:18 -0700

    created allocator header file with methods to be implemented

---
+++ allocator.h
@@ -0,0 +1,26 @@
+// Daniel Domingos (Github: ddomingos1006)
+
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+#define INITIAL_SIZE 1024 * 1024
+#define ALIGNMENT 8
+
+// initialize the allocator and an initially free array of INITIAL_SIZE bytes 
+void init_allocator(void);
+
+// allocate size bytes on the heap, returning a pointer to the allocated block
+void* d_malloc(size_t size);
+
+// free memory previously d_malloc-ed at the address ptr, 
+// undefined behavior for freeing non d_malloced memory
+void d_free(void* ptr);
+
+// reallocate a block of d_malloced-ed memory at the address ptr to the new size,
+// undefined behavior for attempting to realloc non d_malloced memory
+void* d_realloc(void* ptr, size_t new_size);
+
+/* Debug / test helper */
+void print_memory_state(void);
+
+#endif /* ALLOCATOR_H */
